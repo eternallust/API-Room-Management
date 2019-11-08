44ad2b7c7b25022efd5c9fcb8b11fad9fa9b0d1c
eternallust <rizkyabdulrachman@gmail.com>
2019-11-08 18:55:40 +0700

    Update rooms.js

--- models/rooms.js
+++ models/rooms.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const rooms = sequelize.define('rooms', {
     name: DataTypes.STRING,
-    available: DataTypes.BOOLEAN,
+    available: DataTypes.INTEGER,
     type: DataTypes.INTEGER
 
   }, {});
