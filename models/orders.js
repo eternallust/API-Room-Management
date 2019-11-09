eeb8e90bb4af192cbba94b6036c73e577655b4b5
eternallust <rizkyabdulrachman@gmail.com>
2019-11-09 20:38:39 +0700

    Update orders.js

--- models/orders.js
+++ models/orders.js
@@ -3,10 +3,10 @@ module.exports = (sequelize, DataTypes) => {
   const orders = sequelize.define('orders', {
     customer_id: DataTypes.INTEGER,
     room_id: DataTypes.INTEGER,
-    is_booked: DataTypes.BOOLEAN,
+    is_booked: DataTypes.INTEGER,
     duration: DataTypes.INTEGER,
     order_end_time: DataTypes.DATE,
-    is_done : DataTypes.BOOLEAN
+    is_done : DataTypes.INTEGER
   }, {});
   orders.associate = function(models) {
     // associations can be defined here
